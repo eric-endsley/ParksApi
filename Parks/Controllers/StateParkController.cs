491994500a7c071731fcde844ad8cae36f2a9e92
Eric Endsley <eric.endsley4@gmail.com>
2021-01-22 09:57:46 -0800

    create state park controller with read and create functionality

---
+++ Parks/Controllers/StateParkController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Parks.Models;
+
+namespace Parks.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class StateParksController : ControllerBase
+    {
+        private ParksContext _db;
+
+        public StateParksController(ParksContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<StatePark>> Get()
+        {
+            return _db.StateParks.ToList();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] StatePark statePark)
+        {
+            _db.StateParks.Add(statePark);
+            _db.SaveChanges();
+        }
+    }
+}
