db089b93d21032a6b75749aec40e6a50a68b7fac
Eric Endsley <eric.endsley4@gmail.com>
2021-01-22 09:16:23 -0800

    add natlpark and statepark models

---
+++ Parks/Models/NatlPark.cs
@@ -0,0 +1,10 @@
+namespace Parks.Models
+{
+    public class NatlPark
+    {
+        public int NatlParkId { get; set; }
+        public string Name { get; set; }
+
+        public string State { get; set; }
+    }
+}
---
+++ Parks/Models/StatePark.cs
@@ -0,0 +1,10 @@
+namespace Parks.Models
+{
+    public class StatePark
+    {
+        public int StateParkId { get; set; }
+        public string Name { get; set; }
+
+        public string State { get; set; }
+    }
+}
