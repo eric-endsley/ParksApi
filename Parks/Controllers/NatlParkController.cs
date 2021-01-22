6a21b78c22d42a4230738700069f2c68a1f46a0a
Eric Endsley <eric.endsley4@gmail.com>
2021-01-22 14:14:57 -0800

    add user controller with authenticate method

--- Parks/Controllers/NatlParkController.cs
+++ Parks/Controllers/NatlParkController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Parks.Models;
