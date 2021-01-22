702db3cd8b009efb8456392dd4a4ff3522e7f60f
Eric Endsley <eric.endsley4@gmail.com>
2021-01-22 09:29:20 -0800

    complete initial migration

---
+++ Parks/Migrations/20210122172859_Initial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parks.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NatlParks",
+                columns: table => new
+                {
+                    NatlParkId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NatlParks", x => x.NatlParkId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StateParks",
+                columns: table => new
+                {
+                    StateParkId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StateParks", x => x.StateParkId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NatlParks");
+
+            migrationBuilder.DropTable(
+                name: "StateParks");
+        }
+    }
+}
